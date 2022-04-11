ee1aee6f0e4e1e6c81fd9d76438872aefc552b42
e s <egor@starkov.email>
2022-04-12 01:21:29 +0200

    collections are now channel-dependent!

--- soundboardt_collections.py
+++ soundboardt_collections.py
@@ -14,40 +14,48 @@ from soundboardt import Sound, get_sound
 ####################
 
 # These config sanity checks are extremely weak; it might be better to make them more strict and error messages more informative
-if 'soundbank_use_collections' not in cfg.data: 
-    if 'soundbank_collections' not in cfg.data: 
-        cfg.data['soundbank_use_collections'] = False
-        cfg.data['soundbank_collections'] = { }; 
-    else:
-        cfg.data['soundbank_use_collections'] = True
-        print('Warning: config defines some soundboard collections but "soundbank_use_collections" is undefined! Attempting to use the collections anyway')
-
 if 'soundbank_collections' not in cfg.data: 
-    cfg.data['soundbank_collections'] = { }; 
+    SBCOLLECTIONS = {}
     if 'soundbank_use_collections' == True: 
         cfg.data['soundbank_use_collections'] = False; 
         print('Warning: config says to use soundboard collections, but none are defined! Cancelling!')
-
+    elif 'soundbank_use_collections' not in cfg.data: 
+        cfg.data['soundbank_use_collections'] = False
+else:
+    SBCOLLECTIONS = cfg.soundbank_collections
 
 
 #########################
 ###    Collections    ###
 #########################
 
-def playfromsbcollection(msg: Message, sndCollection: str):
-    print(f'Playing a random sound from collection "{sndCollection}"')
-    snd = get_sound(msg.channel_name, rndchoice(sndCollections[sndCollection]))
-    sound = pd_audio.from_file(snd.filepath) + cfg.soundbank_gain
-    pd_play(sound)
-
+def play_collection(channel: str, colln: str):
+    """Play a sound from the required collection in a given channel, with the channel inferred from the chat message"""
+    if colln in SBCOLLECTIONS[channel]:
+        print(f'Playing a random sound from collection "{colln}" in channel "{channel}"')
+        snd = get_sound(channel, rndchoice(SBCOLLECTIONS[channel][colln]))
+        sound = pd_audio.from_file(snd.filepath) + cfg.soundbank_gain
+        pd_play(sound)
+    else: 
+        raise InvalidArgumentsError(reason=f'There is no collection {colln} defined for channel {channel}!',
+            cmd=play_collection)
 
 if cfg.soundbank_use_collections:
-    sndCollections = cfg.soundbank_collections
-    for sndCollection in sndCollections:
-        """This loop creates a command for each collection in sndCollections, using high amounts of jank.
-        Mind the indentation in the exec string!
-        """
-        exec(f"""@Command('{sndCollection}', syntax='', cooldown=cfg.soundbank_cooldown) 
-async def cmd_playfromsbcollection(msg: Message): 
-    playfromsbcollection(msg, '{sndCollection}')
-""")
+    # need to construct (1) full list of collections across all channels and
+    # (2) a list of channels using a given collection
+    # Although is (2) really necessary?
+    collections_list = {}
+    for channel in SBCOLLECTIONS:
+        for colln in SBCOLLECTIONS[channel]:
+            if not colln in collections_list:
+                collections_list[colln] = []
+            collections_list[colln].append(channel)
+            
+    # now just need to create a command for every collection.
+    # I have not found a better way to do this than exec() plus a lot of jank.
+    # !! Mind the indentation in the exec string !!
+    for colln in collections_list:
+        exec(f"""@Command('{colln}', syntax='', cooldown=cfg.soundbank_cooldown) 
+async def cmd_play_collection(msg: Message): 
+    play_collection(msg.channel_name, '{colln}')""")
+
--- soundboardt_hotkeys.py
+++ soundboardt_hotkeys.py
@@ -3,7 +3,8 @@ from pydub.playback import play as pd_play
 from random import choice as rndchoice
 
 from twitchbot import cfg
-from soundboardt import Sound, get_sound
+#from soundboardt import Sound, get_sound
+from soundboardt_collections import play_collection
 
 # pynput is an optional dependency; only attempt to import it if needed
 if 'soundbank_use_hotkeys' not in cfg.data: cfg.data['soundbank_use_hotkeys'] = False
@@ -17,14 +18,18 @@ elif cfg.soundbank_use_hotkeys == True:
 CHANNEL = cfg.channels[0]
 
 
-
+""" the following might still come in handy once collections are integrated in the economy, for the same reason as the next function
 def hotkey_collection(snd_collection_name: str):
-    sndCollection = cfg.soundbank_collections[snd_collection_name]
+    sndCollection = cfg.soundbank_collections[CHANNEL][snd_collection_name]
     snd = get_sound(CHANNEL, rndchoice(sndCollection))
     sound = pd_audio.from_file(snd.filepath) + cfg.soundbank_gain
     pd_play(sound)
+    """
+
 
 def hotkey_sound(snd_name: str):
+    # this is effectively get_sound() minus all the points-related junk
+    # I should probably have a dedicated function for playing a sound instead...
     snd = get_sound(CHANNEL, snd_name)
     if snd.gain:
         gain = snd.gain
@@ -45,7 +50,7 @@ if cfg.soundbank_hotkeys:
 if cfg.soundbank_hotkeys_collections:
     for key, collection in cfg.soundbank_hotkeys_collections.items():
         # Hotkey receiver needs a function with no arguments:
-        exec(f"def hotkey_collection_{collection}(): hotkey_collection('{collection}')")
+        exec(f"def hotkey_collection_{collection}(): play_collection('{CHANNEL}', '{collection}')")
         exec(f"hotkeyDictionary[key] = hotkey_collection_{collection}")
 
 

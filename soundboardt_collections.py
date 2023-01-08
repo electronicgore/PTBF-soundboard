450f2cf1b1c2d0e362e0232c5769f62a635f54e4
e s <egor@starkov.email>
2023-01-08 21:21:14 +0100

    add chat response on collections cooldown + fix collections accounting

--- soundboardt_collections.py
+++ soundboardt_collections.py
@@ -2,7 +2,12 @@ from twitchbot import (
     Command,
     Message,
     cfg,
-    is_command_off_cooldown
+    is_command_off_cooldown,
+    get_time_since_execute,
+    get_command,
+    get_currency_name,
+    get_balance_from_msg,
+    subtract_balance
 )
 from random import choice as rndchoice
 from soundboardt import Sound, get_sound, play_sound
@@ -44,7 +49,7 @@ else:
 ###    Collections    ###
 #########################
 
-def is_channel_sb_off_cooldown(channel: str) -> bool:
+def is_channel_sb_off_cooldown(channel: str) -> (bool, int):
     """Check all soundbank cooldowns, including 'sb' and all collections"""
     checkbox = []
     checkbox.append(is_command_off_cooldown(channel, cfg.prefix + 'sb'))
@@ -52,44 +57,61 @@ def is_channel_sb_off_cooldown(channel: str) -> bool:
         # why this uses a prefix is beyond me...
         checkbox.append(is_command_off_cooldown(channel, cfg.prefix + colln))
     print(checkbox)
-    return all(checkbox)
 
+    if any(checkbox):
+        # also return the time left on the cooldown
+        cooldowns = []
+        for colln in SBCOLLECTIONS[channel]:
+            timer = get_time_since_execute(channel, cfg.prefix + colln)
+            cooldowns.append(get_command(cfg.prefix + colln).cooldown - timer)
 
-def play_collection(channel: str, colln: str):
-    """Play a sound from the required collection in a given channel, with the channel inferred from the chat message"""
-    if colln in SBCOLLECTIONS[channel]:
-        if is_channel_sb_off_cooldown(channel):
-            RNDSND = rndchoice(SBCOLLECTIONS[channel][colln])
-            print(f'Playing random sound "{RNDSND}" from collection "{colln}" in channel "{channel}"')
-            snd = get_sound(channel, RNDSND)
-            play_sound(snd)
-        else:
-            print(f'Soundbank is on cooldown')
-    else:
-        # This can only happen in a multi-channel setup
-        raise InvalidArgumentsError(reason=f'There is no collection {colln} defined for channel {channel}!',
-            cmd=play_collection)
+    return [all(checkbox), max(cooldowns)]
 
 
 async def accounting_collection(msg: Message, colln: str):
     """Expropriate the points from the message author who dared to play a collection sound"""
+    currency = get_currency_name(msg.channel_name).name
+
     if SBCOLL_PRICE==0:
         # Nothing to do if things are free
         return
     else:
-        currency = get_currency_name(msg.channel_name).name
         if get_balance_from_msg(msg).balance < SBCOLL_PRICE:
             raise InvalidArgumentsError(f'{msg.author} tried to play a sound from "{colln}" '
                 f'for {SBCOLL_PRICE} {currency}, but they do not have enough {currency}!')
         subtract_balance(msg.channel_name, msg.author, SBCOLL_PRICE)
 
-        if cfg.soundbank_verbose:
-            await msg.reply(f'{msg.author} played "{snd.sndid}" for {price} {currency}')
+    if cfg.soundbank_verbose:
+        await msg.reply(f'{msg.author} played "{snd.sndid}" for {SBCOLL_PRICE} {currency}')
+
+
+async def play_collection(msg: Message, colln: str) -> None:
+    """The actual command to play a sound from the required collection in a given channel"""
+    channel = msg.channel_name
+
+    if not colln in SBCOLLECTIONS[channel]:
+        # This can only happen in a multi-channel setup
+        raise InvalidArgumentsError(reason=f'There is no collection {colln} defined for channel {channel}!',
+            cmd=play_collection)
+        return
+
+    if not is_channel_sb_off_cooldown(channel)[0] and is_command_off_cooldown(channel, cfg.prefix + colln):
+        # Check if any of the other collection commands are on cooldown
+        # If the command itself is on cooldown then the standard bot response should take care of it.
+        #print(f'Soundbank is on cooldown')
+        await msg.reply(f'The soundbank is on cooldown, seconds left: {is_channel_sb_off_cooldown(channel)[1]}')
+        return
+
+    RNDSND = rndchoice(SBCOLLECTIONS[channel][colln])
+    print(f'Playing random sound "{RNDSND}" from collection "{colln}" in channel "{channel}"')
+    snd = get_sound(channel, RNDSND)
+    play_sound(snd)
+    await accounting_collection(msg, colln)
 
 
 if cfg.soundbank_use_collections:
-    # construct a full list of collections across all channels, to know which commands to create
-    # outputs a dictionary of the form "collection": ("channel1", "channel2", ...)
+    # construct an inverse dictionary of of collections of the form "collection": ("channel1", "channel2", ...)
+    # to know which commands to create
     collections_list = {}
     for channel in SBCOLLECTIONS:
         for colln in SBCOLLECTIONS[channel]:
@@ -103,7 +125,5 @@ if cfg.soundbank_use_collections:
     for colln in collections_list:
         exec(f"""@Command('{colln}', permission=SBCOLL_PERM, syntax='', cooldown=cfg.soundbank_cooldown)
 async def cmd_play_collection(msg: Message):
-    try: await accounting_collection(msg, '{colln}')
-    except: pass
-    else: play_collection(msg.channel_name, '{colln}')""")
+    await play_collection(msg, '{colln}')""")
 

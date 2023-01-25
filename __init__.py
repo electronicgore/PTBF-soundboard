640837c4efba851e410d6555e5ca3e3ea3cd8b0a
e s <egor@starkov.email>
2023-01-26 00:21:09 +0100

    remove old junk from the __init__ file

--- __init__.py
+++ __init__.py
@@ -1,14 +1,3 @@
-from pathlib import Path
-"""
-_bot_package_path = Path()
-
-def _set_bot_package_path(path: str):
-    global _bot_package_path
-    _bot_package_path = Path(path)
-
-_set_bot_package_path(__path__[0])
-"""
 from .soundboardt import *
-#from .soundboardt_collections import *
 from .soundboard_bot import SoundBot
 

bba96670a6585e12a9deb28bfc30c614a1355aef
e s <egor@starkov.email>
2023-01-28 15:51:06 +0100

    actually add the run_soundbot.py file that is advertised in the readme

---
+++ run_soundbot.py
@@ -0,0 +1,6 @@
+# NOTE: copy this from soundbot folder to the root bot folder, and run this script to run the bot.
+
+from soundbot import SoundBot
+
+if __name__ == '__main__':
+    SoundBot().run()
